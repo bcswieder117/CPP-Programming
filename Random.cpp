79d72e95675d040b23653d8c7abba37e5205f25a
Blaine Swieder <bcswieder42@tntech.edu>
2024-10-13 23:08:23 -0500

    Add files via upload

---
+++ Random.cpp
@@ -0,0 +1,21 @@
+/*
+Blaine Swieder 
+CSC 1300: Introductory Program
+October 6th, 2024
+Filename: Random.cpp
+Description: This is a file to test adding a new file to GitHub.
+*/
+
+#include<iostream> 
+#include<string>
+
+using namespace std; 
+
+int main(){
+    cout << "My name is Blaine Swieder. \n" << endl;
+    cout << "My major is Computer Science. \n" << endl;
+    cout << "I am learning how to use Git and GitHub. \n" << endl;
+
+    return 0;
+}
+
